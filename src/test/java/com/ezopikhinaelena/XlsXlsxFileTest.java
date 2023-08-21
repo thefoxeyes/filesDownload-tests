b6815b8030f1c8ea79a6295702b1e0a01b92fe08
Elena Ezopikhina <thefoxeyes@gmail.com>
2023-08-22 00:53:43 +0300

    Added Assertions to the XlsXlsxTest

--- src/test/java/com/ezopikhinaelena/XlsXlsxFileTest.java
+++ src/test/java/com/ezopikhinaelena/XlsXlsxFileTest.java
@@ -1,6 +1,7 @@
 package com.ezopikhinaelena;
 
 import com.codeborne.xlstest.XLS;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
@@ -11,7 +12,7 @@ public class XlsXlsxFileTest {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         InputStream stream = classLoader.getResourceAsStream("175_school.xlsx");
         XLS xlsFile = new XLS(stream);
-        String stringCellValue = xlsFile.excel.getSheetAt(1).getRow(2).getCell(1).getStringCellValue();
-        System.out.println(stringCellValue);
+        String stringCellValue = xlsFile.excel.getSheetAt(1).getRow(3).getCell(1).getStringCellValue();
+        Assertions.assertTrue(stringCellValue.equals("Бондаренко Наталья Николаевна"));
     }
 }
